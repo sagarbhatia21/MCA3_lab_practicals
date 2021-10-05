482d39ecb5388190c4aeda7938c5995d828b7a13
Anustha Chhetri <34373537+himanshuchandola@users.noreply.github.com>
2021-10-05 19:58:03 +0530

    Nested If code to check age for certain things

---
+++ MCA3HLD/20711136 -  Himanshu Chandola/Codes/Nested_If.py
@@ -0,0 +1,13 @@
+# Himanshu Chandola MCA 3 - HLD Campus STD ID-20711136
+
+age = 19
+isGraduated = False
+hasLicense = True
+
+if age >= 18:
+    print("You're 18 or older. You can vote now in Haldwani")
+
+    if isGraduated:
+        print('Welcome to COllege Life')
+    if hasLicense:
+        print('You can apply for driving license too')
