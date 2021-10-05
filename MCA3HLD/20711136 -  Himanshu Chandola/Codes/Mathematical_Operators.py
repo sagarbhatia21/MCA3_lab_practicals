9068795b1e93cac57f8b8950f922cfb3cadb96c3
Anustha Chhetri <34373537+himanshuchandola@users.noreply.github.com>
2021-10-05 17:58:13 +0530

    Program to Perfrom Mathematical Operators Added

---
+++ MCA3HLD/20711136 -  Himanshu Chandola/Codes/Mathematical_Operators.py
@@ -0,0 +1,17 @@
+# Himanshu Chandola MCA 3 - HLD Campus STD ID-20711136
+
+print('Mathematical Operations in Python')
+num1 = int(input('Enter First Number: '))
+num2 = int(input('Enter Second Number '))
+add = num1 + num2
+dif = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+power = num1 ** num2
+modulus = num1 % num2
+print('Sum of ',num1 ,'and' ,num2 ,'is :',add)
+print('Difference of ',num1 ,'and' ,num2 ,'is :',dif)
+print('Product of' ,num1 ,'and' ,num2 ,'is :',mul)
+print('Division of ',num1 ,'and' ,num2 ,'is :',div)
+print('Exponent of ',num1 ,'and' ,num2 ,'is :',power)
+print('Modulus of ',num1 ,'and' ,num2 ,'is :',modulus)
