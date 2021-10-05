cd88a06b293b96c89deb1e49aca5b11ed5fa9aec
Anustha Chhetri <34373537+himanshuchandola@users.noreply.github.com>
2021-10-05 20:14:35 +0530

    Some Tuple Usage examples

---
+++ MCA3HLD/20711136 -  Himanshu Chandola/Codes/tuple_usage.py
@@ -0,0 +1,9 @@
+# Himanshu Chandola MCA 3 - HLD Campus STD ID-20711136
+
+Himanshu_tuple1 = ("Himanshu", 23, "Male")
+print(Himanshu_tuple1)
+
+tuple1 = ("GEU", "GEHU_DEHRADUN", "GEHU_HALDWANI")
+tuple2 = (1, 2, 3)
+print(tuple1)
+print(tuple2)
