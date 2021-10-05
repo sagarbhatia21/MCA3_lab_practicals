45890652a5f9674f92dcbd0eb4702db3e9b6694e
Anustha Chhetri <34373537+himanshuchandola@users.noreply.github.com>
2021-10-05 21:32:59 +0530

    calculate mean without library

---
+++ MCA3HLD/20711136 -  Himanshu Chandola/Codes/Statistical_Concepts_and_Linear_Algebra/mean.py
@@ -0,0 +1,9 @@
+# Himanshu Chandola MCA 3 - HLD Campus STD ID-20711136
+
+n_num = [11, 12, 13, 14, 15]
+n = len(n_num)
+  
+get_sum = sum(n_num)
+mean = get_sum / n
+  
+print("Mean : " + str(mean))
