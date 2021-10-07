7034b2efda7c7252ad624fea9cb1096409c34d9f
Anustha Chhetri <34373537+himanshuchandola@users.noreply.github.com>
2021-10-07 15:48:44 +0530

    searching an element and sorting a List

---
+++ MCA3HLD/20711136 -  Himanshu Chandola/Codes/Search_SortList.py
@@ -0,0 +1,11 @@
+# Himanshu Chandola MCA 3 - HLD Campus STD ID-20711136
+
+str = [110, 21, 18, 38, 36]
+print(sorted(str))  # with sorting
+print(str)  # without sorting
+
+index = str.index(38)
+print(index)
+
+index = str.index(110)
+print(index)
