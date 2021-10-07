58f83e7cd70d78a2f1e6b9c7cbdc0a124e0a09f4
Anustha Chhetri <34373537+himanshuchandola@users.noreply.github.com>
2021-10-07 15:45:03 +0530

    demonstrate the use of the else clause

---
+++ MCA3HLD/20711136 -  Himanshu Chandola/Codes/else_cause.py
@@ -0,0 +1,9 @@
+# Himanshu Chandola MCA 3 - HLD Campus STD ID-20711136
+
+num = int(input("Enter your Number: "))
+
+if num >= 100:
+    print("Number is Greater than 100")
+else:
+    print("Number is Less than 100")
+
